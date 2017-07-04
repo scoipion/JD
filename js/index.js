18bdcd61ab0c83bed1cca4e45d2891977e8115ed
scoipion <scoipion@sina.com>
2017-07-04 14:37:10 +0800

    JD

---
+++ js/index.js
@@ -0,0 +1,40 @@
+window.onload=function(){
+   var oTilte=document.getElementsByClassName("tilte");  
+   function tilte(obj){
+   	var oUl=obj.getElementsByClassName("tabList")[0];
+   	var aLi=oUl.getElementsByTagName("li");
+   	
+   	
+   	
+   }
+   console.log(oTilte.length);
+// console.log(oUl.length);
+   console.log(aLi.length);
+   
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
